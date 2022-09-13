8516c7a0da1f679495e1743fd780c8ae0f78bd8c
ismailhossendev <ismailhossendev@gmail.com>
2022-09-13 19:48:31 +0600

    dev tools - all think recap alhamdulliah

---
+++ dev-tool.js
@@ -0,0 +1,27 @@
+/*
+ডিবাগিং এবং dev tool সম্পর্কে জানতেই হবে।
+
+১. console ট্যাব
+
+২. source ট্যাব
+
+৩. application
+
+৪. Network
+
+৫. Elements
+
+
+*/
+/* 
+  1->  console log,error, warning etc can show on console tab also we can see output on console tab.
+
+  2-> source tab - see all files and code details with error line etc.
+
+  3-> application tab you see localStorage sessonalStorage data etc on application tab
+
+  4-> Network tab - you can see loading time,page view time ,perfermance etc on network tab
+
+  5-> Elements Tab you can see details of html codes,style,box model and dom,event handeler etc.
+
+*/
